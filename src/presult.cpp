fc29a2ea5f25066797326be37298b7efd118d70a
Jianyang Sun <losttemple101@qq.com>
2015-02-15 16:44:28 -0800

    Update presult.cpp

--- src/presult.cpp
+++ src/presult.cpp
@@ -1,5 +1,5 @@
 #include "ls.h"
-int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
+int Print_result(char *listname[MAXNUM],int k,int flag_dir0,int Round ,int a, int R, int l,int flag_dir_doc)
 {
 	//printf("PR_al listname: ");
 	allpt(k,listname);
@@ -22,34 +22,71 @@ int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
 	//printf("directory name: ");
 	allpt(k-n1,dir_name);
 
-	int flag_dir_doc=0;
+	//int flag_dir_doc=0;
 
 	if(n1>1)mysort(doc_name,n1);
 	//printf("PR_al doc_name ");
 	allpt(n1,doc_name);
 
-	char*tmpdoc[MAXNUM];
+	char *tmpdoc[MAXNUM];
+
 	int if_doc=0;//if doc is display
+	
 	for (int i2=0;i2<n1;i2++)	
 	{
-		//if(doc_name[i2][0]!='.')
-		//{
-			//lprint(doc_name[i2]);
-			//printf("%s\n", doc_name[i2]);
-		tmpdoc[i2]=doc_name[i2];
-			if_doc++;
-		/*}
-		else
+		if(a==0)
+		{	if(doc_name[i2][0]!='.')
+			{
+				if(l==1)
+				{
+					tmpdoc[i2]=doc_name[i2];
+				}
+				else if(l==0)
+				{
+					printf("%s\t", doc_name[i2]);
+				}
+				else
+				{
+					printf("error\n!");
+					exit(1);
+				}
+				if_doc++;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		else if(a==1)
 		{
-			continue;
-		}*/
+			if(l==1)
+			{
+				tmpdoc[i2]=doc_name[i2];
+			}
+			else if(l==0)
+			{
+				printf("%s\t", doc_name[i2]);
+			}
+			else
+			{
+				printf("error\n!");
+				exit(1);
+			}
+			if_doc++;
+		}
 	}
-	lform(if_doc, tmpdoc, tmpdoc);
 
-	/*if(if_doc)//end of doc, without -l
+	if( l==1 )
 	{
-		printf("\n");//all doc display finishes
-	}*/
+		lform(if_doc, tmpdoc, tmpdoc);
+	}
+	else if( l==0 )
+	{
+		if(if_doc)//end of doc, without -l
+		{
+			printf("\n");//all doc display finishes
+		}
+	}
 
 
 
@@ -78,28 +115,37 @@ int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
 			int i3=0;
 			while ((entry = readdir(dirptr))!=NULL)//error check??
 			{								
-				//if(entry->d_name[0]!='.')
-				//{
+				if(a==0)
+				{
+					if(entry->d_name[0]!='.')
+					{
+						sort_dir[i3]=entry->d_name;
+						i3++;
+					}							
+				}
+				else if(a==1)
+				{
 					sort_dir[i3]=entry->d_name;
 					i3++;
-				//}							
-				//printf("%s\t", entry->d_name);
+				}
 			}
 			if(i3>1)mysort(sort_dir,i3);
-			if((Rround==1)||((k-n1>1)&&(i3)))//newline between dir and dir
+			if((Round==1)||((k-n1>1)&&(i3)))//newline between dir and dir
 			{
 				printf("\n");
 			}
+
 			if((i3)&&(flag_dir_doc==0)&&(if_doc))//newline between dor and dir
 			{
 				printf("\n");//all doc display finishes
 				flag_dir_doc=1;
 			}
-			printf("%s:\n",dir_name[i2]);
+			if(R==1 || ( (if_doc+k-n1)>=2 && i3!=0 ) )printf("%s:\n",dir_name[i2]);
 			
 
 			char tmp[MAXNUM][MAXLINE];
 			char *tmpp[MAXNUM];
+			
 			for(int i4=0;i4<i3;i4++)
 			{
 				if(flag_dir0==1)
@@ -114,10 +160,21 @@ int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
 				}
 				strcat(tmp[i4],sort_dir[i4]);
 				tmpp[i4]=tmp[i4];
-				//lprint(tmp[i4]);
-				//printf("%s\n",sort_dir[i4]);
+				if(l==0)
+				{
+					printf("%s\t",sort_dir[i4]);
+				}
 			}
-			lform(i3, tmpp,sort_dir);
+			if( l==0 )
+			{
+				if(i3)//end of doc, without -l
+				{
+					printf("\n");//all doc display finishes
+				}
+			}
+
+			if(l==1)lform(i3, tmpp,sort_dir);
+
 			if(i3)
 			{
 				char *tmp_arr[MAXNUM];
@@ -138,11 +195,14 @@ int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
 				{
 					tmp_dir[i-n11]=tmp_arr[i];
 				}
+
 				//printf("directory name: ");
 				allpt(i5-n11,tmp_dir);
-				Rround=1;
-				if(i5-n11)PR_al( tmp_dir,i5-n11, flag_dir0,Rround);
+
+				Round=1;
+				if(R==1 && (i5-n11) )Print_result( tmp_dir,i5-n11, flag_dir0,Round,a,R,l,flag_dir_doc);
 			}
+
 			if(closedir(dirptr)!=0)
 			{
 				perror("closedir error!\n"); 
@@ -155,467 +215,3 @@ int PR_al(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
 	}
 	return 0;	
 }
-
-int PR_l(char *listname[MAXNUM],int k,int flag_dir0,int Rround)//lR
-{
-
-	//printf("PR_l listname: ");
-	allpt(k,listname);
-
-
-	const int n1=sp_dir(k,listname);
-	char *doc_name[MAXNUM];//n1 documents
-	for(int i=0;i<n1;i++)
-	{
-		doc_name[i]=listname[i];
-	}
-	//printf("document name: ");
-	allpt(n1, doc_name);
-
-	char *dir_name[MAXNUM];//[k-n1]all the parameter
-	for(int i=n1;i<k;i++)
-	{
-		dir_name[i-n1]=listname[i];
-	}
-	//printf("directory name: ");
-	allpt(k-n1,dir_name);
-
-	int flag_dir_doc=0;
-
-	if(n1>1)mysort(doc_name,n1);
-	//printf("PR_l doc_name ");
-	allpt(n1,doc_name);
-
-	char*tmpdoc[MAXNUM];
-	int if_doc=0;//if doc is display
-	for (int i2=0;i2<n1;i2++)	
-	{
-		if((doc_name[i2][0]!='.')||((doc_name[i2][0]=='.') &&( doc_name[i2][1]=='/') ))
-		{
-			//lprint(doc_name[i2]);
-			//printf("%s\n", doc_name[i2]);
-			tmpdoc[i2]=doc_name[i2];
-			if_doc++;
-		}
-		else
-		{
-			continue;
-		}
-	}
-	lform(if_doc, tmpdoc, tmpdoc);
-
-
-	/*if(if_doc)//end of doc, without -l
-	{
-		printf("\n");//all doc display finishes
-	}*/
-
-
-
-	if(k-n1>1)mysort(dir_name,k-n1);
-	allpt(k-n1,dir_name);
-	for (int i2=0;i2<k-n1;i2++)
-	{
-		DIR *dirptr = NULL;
-		struct dirent *entry;
-		struct stat s;	
-		char *sort_dir[MAXNUM];
-
-		if(stat(dir_name[i2],&s)!=0)
-		{
-			perror("stat dir goes wrong!\n");
-			exit(1);
-		}
-	
-		if((dirptr = opendir(dir_name[i2])) == NULL)//errot check
-		{
-			perror("open dir error !\n");
-			exit (1);
-		}
-		else//normal display
-		{
-			int i3=0;
-			while ((entry = readdir(dirptr))!=NULL)//error check??
-			{								
-				if(entry->d_name[0]!='.')
-				{
-					sort_dir[i3]=entry->d_name;
-					i3++;
-				}							
-			}
-			if(i3>1)mysort(sort_dir,i3);
-			if((Rround==1)||((k-n1>1)&&(i3)))//newline between dir and dir, (k>1,if_doc=0->flag=0,need newline)(k=1,if_doc>0->
-			{
-				printf("\n");
-			}
-			if((i3)&&(flag_dir_doc==0)&&(if_doc))//newline between dor and dir
-			{
-				printf("\n");//all doc display finishes
-				flag_dir_doc=1;
-			}
-
-			printf("%s:\n",dir_name[i2]);
-			
-			char tmp[MAXNUM][MAXLINE];
-			char *tmpp[MAXNUM];
-			for(int i4=0;i4<i3;i4++)
-			{
-				if(flag_dir0==1)
-				{
-					strcpy(dir_name[i2],".");
-					flag_dir0=0;
-				}		
-				strcpy(tmp[i4],dir_name[i2]);
-				if(dir_name[i2][strlen(dir_name[i2])-1]!='/')
-				{
-					strcat(tmp[i4],"/");
-				}
-				strcat(tmp[i4],sort_dir[i4]);
-				tmpp[i4]=tmp[i4];
-			}
-			lform(i3, tmpp,sort_dir);
-			
-			if(i3)
-			{
-				char *tmp_arr[MAXNUM];
-				int i5=0;
-				for (int i=0;i<i3;i++)
-				{
-					if(sort_dir[i][0]!='.')
-					{
-						tmp_arr[i5]=tmp[i];
-						i5++;
-					}
-				}
-				
-				const int n11=sp_dir(i5,tmp_arr);
-				
-				char *tmp_dir[MAXNUM];//[i3-n11]all the parameter
-				for(int i=n11;i<i5;i++)
-				{
-					tmp_dir[i-n11]=tmp_arr[i];
-				}
-				//printf("directory name: ");
-				allpt(i5-n11,tmp_dir);
-				Rround=1;
-				if(i5-n11)PR_l( tmp_dir,i5-n11, flag_dir0,Rround);
-			}
-			if(closedir(dirptr)!=0)
-			{
-				perror("closedir error!\n"); 
-				exit(1);
-			}//error check
-			
-			
-		}
-
-	}
-	return 0;	
-}
-
-int PR_a(char *listname[MAXNUM],int k,int flag_dir0,int Rround)
-{
-	//printf("PR_a listname: ");
-	allpt(k,listname);
-
-
-	const int n1=sp_dir(k,listname);
-	char *doc_name[MAXNUM];//n1 documents
-	for(int i=0;i<n1;i++)
-	{
-		doc_name[i]=listname[i];
-	}
-	//printf("document name: ");
-	allpt(n1, doc_name);
-
-	char *dir_name[MAXNUM];//[k-n1]all the parameter
-	for(int i=n1;i<k;i++)
-	{
-		dir_name[i-n1]=listname[i];
-	}
-	//printf("directory name: ");
-	allpt(k-n1,dir_name);
-
-	int flag_dir_doc=0;
-
-	if(n1>1)mysort(doc_name,n1);
-	//printf("PR_al doc_name ");
-	allpt(n1,doc_name);
-	int if_doc=0;//if doc is display
-	for (int i2=0;i2<n1;i2++)	
-	{
-		//if(doc_name[i2][0]!='.')
-		//{
-			//lprint(doc_name[i2]);
-			printf("%s\t", doc_name[i2]);
-			if_doc++;
-		/*}
-		else
-		{
-			continue;
-		}*/
-	}
-
-	if(if_doc)//end of doc
-	{
-		printf("\n");//all doc display finishes
-	}
-
-
-
-	if(k-n1>1)mysort(dir_name,k-n1);
-	allpt(k-n1,dir_name);
-	for (int i2=0;i2<k-n1;i2++)
-	{
-		DIR *dirptr = NULL;
-		struct dirent *entry;
-		struct stat s;	
-		char *sort_dir[MAXNUM];
-
-		if(stat(dir_name[i2],&s)!=0)
-		{
-			perror("stat dir goes wrong!\n");
-			exit(1);
-		}
-	
-		if((dirptr = opendir(dir_name[i2])) == NULL)//errot check
-		{
-			perror("open dir error !\n");
-			exit (1);
-		}
-		else//normal display
-		{
-			int i3=0;
-			while ((entry = readdir(dirptr))!=NULL)//error check??
-			{								
-				//if(entry->d_name[0]!='.')
-				//{
-					sort_dir[i3]=entry->d_name;
-					i3++;
-				//}							
-				//printf("%s\t", entry->d_name);
-			}
-			if(i3>1)mysort(sort_dir,i3);
-			if((Rround==1)||((k-n1>1)&&(i3)))//newline between dir and dir
-			{
-				printf("\n");
-			}
-			if((i3)&&(flag_dir_doc==0)&&(if_doc))//newline between dor and dir
-			{
-				printf("\n");//all doc display finishes
-				flag_dir_doc=1;
-			}
-
-			printf("%s:\n",dir_name[i2]);
-			
-			char tmp[MAXNUM][MAXLINE];
-			
-			for(int i4=0;i4<i3;i4++)
-			{
-				if(flag_dir0==1)
-				{
-					strcpy(dir_name[i2],".");
-					flag_dir0=0;
-				}		
-				strcpy(tmp[i4],dir_name[i2]);
-				if(dir_name[i2][strlen(dir_name[i2])-1]!='/')
-				{
-					strcat(tmp[i4],"/");
-				}
-				strcat(tmp[i4],sort_dir[i4]);
-				//lprint(tmp[i4]);
-				printf("%s\t",sort_dir[i4]);
-			}
-			if(i3)
-			{
-				printf("\n");
-				char *tmp_arr[MAXNUM];
-				int i5=0;
-				for (int i=0;i<i3;i++)//. .. should not be used
-				{
-					if( (strcmp(sort_dir[i],".")!=0)&&(strcmp(sort_dir[i],"..")!=0) )
-					{
-						tmp_arr[i5]=tmp[i];
-						i5++;
-					}
-				}
-
-				const int n11=sp_dir(i5,tmp_arr);//n11 doc in spdir
-			
-				char *tmp_dir[MAXNUM];//[i3-n11]all the dir
-				for(int i=n11;i<i5;i++)
-				{
-					tmp_dir[i-n11]=tmp_arr[i];
-				}
-				//printf("directory name: ");
-				allpt(i5-n11,tmp_dir);
-				Rround=1;
-				if(i5-n11)PR_a( tmp_dir,i5-n11, flag_dir0,Rround);
-			}
-			if(closedir(dirptr)!=0)
-			{
-				perror("closedir error!\n"); 
-				exit(1);
-			}//error check
-			
-			
-		}
-
-	}
-	return 0;	
-}
-
-
-int PR(char *listname[MAXNUM],int k,int flag_dir0,int Rround)//R
-{
-
-	//printf("PR listname: ");
-	allpt(k,listname);
-
-
-	const int n1=sp_dir(k,listname);
-	char *doc_name[MAXNUM];//n1 documents
-	for(int i=0;i<n1;i++)
-	{
-		doc_name[i]=listname[i];
-	}
-	//printf("document name: ");
-	allpt(n1, doc_name);
-
-	char *dir_name[MAXNUM];//[k-n1]all the parameter
-	for(int i=n1;i<k;i++)
-	{
-		dir_name[i-n1]=listname[i];
-	}
-	//printf("directory name: ");
-	allpt(k-n1,dir_name);
-
-	int flag_dir_doc=0;
-
-	if(n1>1)mysort(doc_name,n1);
-	//printf("PR_al doc_name ");
-	allpt(n1,doc_name);
-	int if_doc=0;//if doc is display
-	for (int i2=0;i2<n1;i2++)	
-	{
-		if((doc_name[i2][0]!='.')||((doc_name[i2][0]=='.') &&( doc_name[i2][1]=='/') ))
-		{
-			//lprint(doc_name[i2]);
-			printf("%s\t", doc_name[i2]);
-			if_doc++;
-		}
-		else
-		{
-			continue;
-		}
-	}
-
-	if(if_doc)//end of doc, without -l
-	{
-		printf("\n");//all doc display finishes
-	}
-
-
-
-	if(k-n1>1)mysort(dir_name,k-n1);
-	allpt(k-n1,dir_name);
-	for (int i2=0;i2<k-n1;i2++)
-	{
-		DIR *dirptr = NULL;
-		struct dirent *entry;
-		struct stat s;	
-		char *sort_dir[MAXNUM];
-
-		if(stat(dir_name[i2],&s)!=0)
-		{
-			perror("stat dir goes wrong!\n");
-			exit(1);
-		}
-	
-		if((dirptr = opendir(dir_name[i2])) == NULL)//errot check
-		{
-			perror("open dir error !\n");
-			exit (1);
-		}
-		else//normal display
-		{
-			int i3=0;
-			while ((entry = readdir(dirptr))!=NULL)//error check??
-			{								
-				if(entry->d_name[0]!='.')
-				{
-					sort_dir[i3]=entry->d_name;
-					i3++;
-				}							
-			}
-			if(i3>1)mysort(sort_dir,i3);
-			if((Rround==1)||((k-n1>1)&&(i3)))//newline between dir and dir, (k>1,if_doc=0->flag=0,need newline)(k=1,if_doc>0->
-			{
-				printf("\n");
-			}
-			if((i3)&&(flag_dir_doc==0)&&(if_doc))//newline between dor and dir
-			{
-				printf("\n");//all doc display finishes
-				flag_dir_doc=1;
-			}
-
-			printf("%s:\n",dir_name[i2]);
-			
-
-			char tmp[MAXNUM][MAXLINE];
-			
-			for(int i4=0;i4<i3;i4++)
-			{
-				if(flag_dir0==1)
-				{
-					strcpy(dir_name[i2],".");
-					flag_dir0=0;
-				}		
-				strcpy(tmp[i4],dir_name[i2]);
-				if(dir_name[i2][strlen(dir_name[i2])-1]!='/')
-				{
-					strcat(tmp[i4],"/");
-				}
-				strcat(tmp[i4],sort_dir[i4]);
-				//lprint(tmp[i4]);
-				printf("%s\t",sort_dir[i4]);
-			}
-			if(i3)
-			{
-				printf("\n");
-				char *tmp_arr[MAXNUM];
-				int i5=0;
-				for (int i=0;i<i3;i++)
-				{
-					if(sort_dir[i][0]!='.')
-					{
-						tmp_arr[i5]=tmp[i];
-						i5++;
-					}
-				}
-				
-				const int n11=sp_dir(i5,tmp_arr);
-				
-				char *tmp_dir[MAXNUM];//[i3-n11]all the parameter
-				for(int i=n11;i<i5;i++)
-				{
-					tmp_dir[i-n11]=tmp_arr[i];
-				}
-				//printf("directory name: ");
-				allpt(i5-n11,tmp_dir);
-				Rround=1;
-				if(i5-n11)PR( tmp_dir,i5-n11, flag_dir0,Rround);
-			}
-			if(closedir(dirptr)!=0)
-			{
-				perror("closedir error!\n"); 
-				exit(1);
-			}//error check
-			
-			
-		}
-
-	}
-	return 0;	
-}
-
